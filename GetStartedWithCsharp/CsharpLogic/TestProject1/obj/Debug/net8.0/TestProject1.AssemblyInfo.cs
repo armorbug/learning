81ddea667248b79bdad6d3c639dc50f8e224cd56
armorbug <vvladi30@gmail.com>
2024-10-31 11:55:44 +0200

    Methods with parameters

--- GetStartedWithCsharp/CsharpLogic/TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
+++ GetStartedWithCsharp/CsharpLogic/TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f436aafca752850e7794c81c32122f4b0102a8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cff035f64fc8456b6df51fcd49bd35a6e0d2ef63")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
